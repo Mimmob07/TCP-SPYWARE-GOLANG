0be62e28c8252174d5aa50a7a70153449eb70651
Pineman834 <mimmo.baluyut@icloud.com>
2023-07-18 22:16:27 -0400

    Added Payload Transfer

--- client/main.go
+++ client/main.go
@@ -1,42 +1,59 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "net"
-        "os"
-        "strings"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
-	HOST = "localhost"
-	PORT = "8080"
-	TYPE = "tcp"
+	HOST       = "localhost"
+	PORT       = "8080"
+	TYPE       = "tcp"
+	BUFFERSIZE = 1024
 )
 
+var logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+
 func main() {
-        CONNECT := HOST+":"+PORT
-        conn, err := net.Dial(TYPE, CONNECT)
-        handleError(err)
+	CONNECT := HOST + ":" + PORT
+	conn, err := net.Dial(TYPE, CONNECT)
+	handleError(err)
+	defer conn.Close()
 
-        for {
-                reader := bufio.NewReader(os.Stdin)
-                fmt.Print(">> ")
-                text, _ := reader.ReadString('\n')
-                fmt.Fprintf(conn, text+"\n")
+	bufferFileName := make([]byte, 64)
+	bufferFileSize := make([]byte, 10)
 
-                message, _ := bufio.NewReader(conn).ReadString('\n')
-                fmt.Print("->: " + message)
-                if strings.TrimSpace(string(text)) == "STOP" {
-                        fmt.Println("TCP client exiting...")
-                        return
-                }
+	conn.Read(bufferFileSize)
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+
+	conn.Read(bufferFileName)
+	fileName := strings.Trim(string(bufferFileName), ":")
+
+	createPayload, err := os.Create(fileName)
+	handleError(err)
+	defer createPayload.Close()
+
+	var receivedBytes int64
+
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(createPayload, conn, (fileSize - receivedBytes))
+			conn.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+			break
+		}
+		io.CopyN(createPayload, conn, BUFFERSIZE)
+		receivedBytes += BUFFERSIZE
 	}
+	logger.Println("Received file completely!")
 }
 
 func handleError(err error) {
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
+	if err != nil {
+		logger.Println("Error: ", err)
+		return
+	}
 }
--- server/main.go
+++ server/main.go
@@ -1,43 +1,78 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"io"
+	"log"
 	"net"
-	"strings"
-	"time"
+	"os"
+	"strconv"
 )
 
 const (
-	HOST = "localhost"
-	PORT = "8080"
-	TYPE = "tcp"
+	HOST       = "localhost"
+	PORT       = "8080"
+	TYPE       = "tcp"
+	BUFFERSIZE = 1024
 )
 
-func main() {
-        listen, err := net.Listen(TYPE, HOST+":"+PORT)
-        handleError(err)
-        defer listen.Close()
-        conn, err := listen.Accept()
-        handleError(err)
-
-        for {
-                netData, err := bufio.NewReader(conn).ReadString('\n')
-                handleError(err)
-                if strings.TrimSpace(string(netData)) == "STOP" {
-                        fmt.Println("Exiting TCP server!")
-                        return
-                }
-                
-                fmt.Print("-> ", string(netData))
-                myTime := time.Now().Format(time.RFC3339) + "\n"
-                conn.Write([]byte(myTime))
-        }
-}
+var count = 0
+var logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 
 func handleError(err error) {
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
+	if err != nil {
+		logger.Println("Panic: ", err)
+		return
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	for {
+		payload, err := os.Open("payload.txt")
+		handleError(err)
+		fileInfo, err := payload.Stat()
+		handleError(err)
+		defer conn.Close()
+		fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+		fileName := fillString(fileInfo.Name(), 64)
+		conn.Write([]byte(fileSize))
+		conn.Write([]byte(fileName))
+		sendBuffer := make([]byte, BUFFERSIZE)
+		for {
+			_, err = payload.Read(sendBuffer)
+			if err == io.EOF {
+				break
+			}
+			conn.Write(sendBuffer)
+		}
+		logger.Println("File sent in theory")
+		conn.Close()
+		break
+	}
+}
+
+func fillString(retunString string, toLength int) string { // Fill string with bytes
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
+}
+
+func main() {
+
+	server, err := net.Listen(TYPE, HOST+":"+PORT)
+	handleError(err)
+	defer server.Close()
+	logger.Println("Server is now listening")
+
+	for {
+		conn, err := server.Accept()
+		handleError(err)
+		go handleConnection(conn)
+		count++
+	}
 }
